a8106ba134d79cf17f344318b6c2afaa18949b78
aalanizf04 <a.alaniz18@hotmail.com>
2018-08-24 15:20:45 -0300

    agregando uso de back en front

--- cliente/musify/src/app/app.component.ts
+++ cliente/musify/src/app/app.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
-import {User} from './models/user'
+import { Component, OnInit } from '@angular/core';
+import {User} from './models/user';
+import {UserService} from './services/user.services';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [UserService]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   public title = 'Musify';
   public user: User;
   public identity;
   public token;
 
-  constructor(){
+  constructor(private _userService: UserService){
   	this.user= new User('','','','','','ROLE_USER','');
 
+  }
+
+  public onSubmit(){
+  	console.log(this.user);
+    this._userService.signup(this.user).subscribe(
+      response=>{
+        console.log(response);
+      })/*
+      error=>{
+        var errorMessage=<any> error;
+        if(errorMessage!=null){
+        console.log(error);}
+      }
+      );*/
+  }
+
+  public ngOnInit(){
+    console.log();
   }
 }
---
+++ cliente/musify/src/app/services/user.services.ts
@@ -0,0 +1,26 @@
+//Clase de servicio, para hacer peticiones a la APIREST, que interactua con los metodos de la Api relacionados con los usuarios
+
+import {Injectable} from '@angular/core';
+import {Response} from '@angular/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {GLOBAL} from './global';
+
+@Injectable() //para inyeccion de dependencias
+export class UserService{
+	
+	public url: string;
+
+	constructor(private _http: HttpClient){
+		this.url=GLOBAL.url;
+	}
+
+	public signup(userToLogin, getHash=null){
+		let params = JSON.stringify(userToLogin);
+		let headers = new HttpHeaders({'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'});
+
+		return this._http.post(this.url+'/login',params,{headers:headers});
+	}
+
+}
+
